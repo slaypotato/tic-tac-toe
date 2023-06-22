7ff7d0631bb0dff9a5ed981b1fe167a289245c4a
Rafael Morales <rafael_c_morales@yahoo.com.br>
2023-06-21 22:09:41 -0300

    added return back in time

--- src/App.js
+++ src/App.js
@@ -2,14 +2,37 @@ import { useState } from 'react';
 import Board from './components/board';
 
 export default function App() {
-  const [xIsNext, setXIsNext] = useState(true);
+  //const [xIsNext, setXIsNext] = useState(true);
   const [history, setHistory] = useState([Array(9).fill(null)]);
-  const currentSquares = history[history.length - 1];
+  const [currentMove, setCurrentMove] = useState(0);
+  const xIsNext = currentMove % 2 === 0;
+  const currentSquares = history[currentMove];
 
   function handlePlay(nextSquares) {
-    setHistory([...history, nextSquares]);
-    setXIsNext(!xIsNext);
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
+    //setXIsNext(!xIsNext);
   }
+
+  function jumpTo(nextMove) {
+    setCurrentMove(nextMove);
+    //setXIsNext(nextMove % 2 === 0);
+  }
+
+  const moves = history.map((squares, move) => {
+    let description;
+    if (move > 0) {
+      description = 'Go to move #' + move;
+    } else {
+      description = 'Go to game start';
+    }
+    return (
+      <li key={move}>
+        <button className='history' onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    );
+  });
 
   return( 
     <>
@@ -21,7 +44,11 @@ export default function App() {
         />
       </div>
       <div className='game'>
-        <div className='status'>Teste</div>
+        <div className='status'></div>
+        <div className='status'>History</div>
+        <div>
+          <ol>{moves}</ol>
+        </div>
       </div>
     </>
 
